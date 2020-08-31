2f15e1b9f7b8c613e722adac9eeb871b69ee64c1
Victor Augusto <44125400+vvaugusto@users.noreply.github.com>
2020-08-30 23:33:22 -0300

    integração com servidor de autorização

---
+++ src/main/java/br/com/itviclabs/restaurante/config/ResourceServerConfigurer.java
@@ -0,0 +1,21 @@
+package br.com.itviclabs.restaurante.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+public class ResourceServerConfigurer extends ResourceServerConfigurerAdapter {
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, "/produto")
+			.permitAll()
+			.antMatchers(HttpMethod.GET, "/pedido")
+			.permitAll()
+			.anyRequest()
+			.authenticated();
+	}
+}
--- src/main/java/br/com/itviclabs/restaurante/rest/PedidoEndpoint.java
+++ src/main/java/br/com/itviclabs/restaurante/rest/PedidoEndpoint.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,10 +18,12 @@ import br.com.itviclabs.restaurante.dto.ConfirmacaoPedido;
 import br.com.itviclabs.restaurante.dto.PedidoRequest;
 import br.com.itviclabs.restaurante.exception.ProdutosInvalidosException;
 import br.com.itviclabs.restaurante.service.PedidoService;
+import lombok.extern.slf4j.Slf4j;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/pedido")
+@Slf4j
 public class PedidoEndpoint {
 
 	@Autowired
@@ -28,6 +31,8 @@ public class PedidoEndpoint {
 	
 	@PostMapping
 	public ConfirmacaoPedido efetuarPedido(@RequestBody PedidoRequest pedidoRequest) throws ProdutosInvalidosException {
+		log.info("Authenticated user: " + SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+		log.info("efetuarPedido: {}", pedidoRequest);
 		return pedidoService.efetuarPedido(pedidoRequest);
 	}
 	
